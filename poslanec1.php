be260b4c034fa2178d588c3314d78bde2bdfc8ca
Ondrej06 <129960184+Ondrej06@users.noreply.github.com>
2024-04-30 16:58:09 +0400

    Update poslanec1.php
    foto

--- poslanec1.php
+++ poslanec1.php
@@ -11,11 +11,11 @@
 <body>
   <div class="nadpis"><h1>POSLANCI ČESKÉ REPUBLIKY</h1></div>
   <div class="poslanci_karticky"><a href="http://127.0.0.1/%c5%a1kola%20projekt/Babi%c5%a1.php"><img src="Andrej_Babiš_v_roce_2020_(cropped).jpg"></div></a>
-  <div class="poslanci_karticky"><a href="http://127.0.0.1/%c5%a1kola%20projekt/Fiala.php"><img src="Petr_Fiala_2022.jpg"></div></a>
-  <div class="poslanci_karticky"><a href="http://127.0.0.1/%c5%a1kola%20projekt/Okamura.php"><img src="images.jpg"></div></a>
-  <div class="poslanci_karticky"><a href="http://127.0.0.1/%c5%a1kola%20projekt/Barto%c5%a1.php"><img src="Ivan_Bartoš_16_December_2021.jpg"></div></a>
-  <div class="poslanci_karticky"><a href="http://127.0.0.1/%c5%a1kola%20projekt/Filip.php"><img src="image.jpg"></div></a>
-  <div class="poslanci_karticky"><a href="http://127.0.0.1/%c5%a1kola%20projekt/Pekarov%c3%a1.php"><img src="Markéta_Pekarová_Adamová_(2022).jpg"></div></a>
-  <div class="poslanci_karticka"><a href="http://127.0.0.1/%c5%a1kola%20projekt/Jure%c4%8dka.php"><img src="i6205.jpg"></div> </a>
+  <div class="poslanci_karticky"><a href="http://127.0.0.1/%c5%a1kola%20projekt/Fiala.php"><img src="assets/images/Petr_Fiala_2022.jpg"></div></a>
+  <div class="poslanci_karticky"><a href="http://127.0.0.1/%c5%a1kola%20projekt/Okamura.php"><img src="assets/images/images.jpg"></div></a>
+  <div class="poslanci_karticky"><a href="http://127.0.0.1/%c5%a1kola%20projekt/Barto%c5%a1.php"><img src="assets/images/Ivan_Bartoš_16_December_2021.jpg"></div></a>
+  <div class="poslanci_karticky"><a href="http://127.0.0.1/%c5%a1kola%20projekt/Filip.php"><img src="assets/images/image.jpg"></div></a>
+  <div class="poslanci_karticky"><a href="http://127.0.0.1/%c5%a1kola%20projekt/Pekarov%c3%a1.php"><img src="assets/images/Markéta_Pekarová_Adamová_(2022).jpg"></div></a>
+  <div class="poslanci_karticka"><a href="http://127.0.0.1/%c5%a1kola%20projekt/Jure%c4%8dka.php"><img src="assets/images/i6205.jpg"></div> </a>
 </body>
 </html>

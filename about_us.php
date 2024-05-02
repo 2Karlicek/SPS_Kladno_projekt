a8de2cae7a5a1a77d19a397f197df4b69b224ab5
Ondrej06 <129960184+Ondrej06@users.noreply.github.com>
2024-05-03 00:29:40 +0400

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>O NÁS - Volební Kalkulačky</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+            color: #333;
+        }
+        header {
+            background-color: #007bff;
+            color: #fff;
+            text-align: center;
+            padding: 20px;
+        }
+        .container {
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+            margin-top: 0;
+        }
+        img{
+            width: 300px;
+            display: inline-block;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>O NÁS - Volební Kalkulačky</h1>
+    </header>
+    <div class="container">
+        <h2>Vítejte na našem webu!</h2>
+        <p>Jsme tým nadšenců, kteří věří, že informovanost je klíčová pro demokracii. Naše vášeň spočívá v poskytování nástrojů, které pomáhají voličům lépe porozumět politickým stranám a jejich programům.</p>
+        <h2>Co Děláme</h2>
+        <p>Naše hlavní služba je volební kalkulačka. Pomáháme voličům identifikovat stranu, která nejlépe odráží jejich politické názory a preference.</p>
+        <h2>Proč Používat Naši Kalkulačku</h2>
+        <ul>
+            <li>Objektivní analýza</li>
+            <li>Informovanost</li>
+            <li>Demokratická angažovanost</li>
+        </ul>
+        <h2>Jak Nás Podpořit</h2>
+        <ul>
+            <li>Používejte Naši Kalkulačku</li>
+            <li>Sdílejte S Námi Vaše Zkušenosti</li>
+            <li>Podporou</li>
+            <img src="https://xeon.spskladno.cz/~novotnyo/%c5%a1kola%20projekt/SPS_Kladno_projekt-main/assets/images/Untitled%201.png">
+        </ul>
+        <p>Děkujeme, že jste s námi na cestě k větší transparentnosti a účasti v politickém procesu!<br> </p>
+    </div>
+</body>
+</html>
